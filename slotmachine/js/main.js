5f6a699dae303efc618eaca06e99b3b54321af7c
delpillar <knivex27@gmail.com>
2014-10-14 12:46:25 -0400

    create function to preload images for canvas

--- slotmachine/js/main.js
+++ slotmachine/js/main.js
@@ -41,6 +41,27 @@ function init() {
     
 };
 
+// function to preload images before drawing on canvas.
+function loadImages(sources, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+
+    //get num of sources
+    for (var src in sources) {
+        numImages++;
+    }
+    for (var src in sources) {
+        images[src] = new Image();
+        images[src].onload = function () {
+            if (loadedImages++ >= numImages) {
+                callback(images);
+            }
+        };
+        images[src].src = sources[src];
+    }
+}
+
 function drawBitmap(img1, img2, img3) {
     
     
